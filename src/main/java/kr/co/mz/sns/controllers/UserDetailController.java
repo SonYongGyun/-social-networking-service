96a5438964c010fffe7dd411bb5b42d8e77abd35
mz01-omega <omega@mz.co.kr>
2023-07-25 15:27:54 +0900

    add principal login to UserDetailService.java

--- src/main/java/kr/co/mz/sns/controllers/UserDetailController.java
+++ src/main/java/kr/co/mz/sns/controllers/UserDetailController.java
@@ -4,6 +4,8 @@ import kr.co.mz.sns.dto.UserDetailDto;
 import kr.co.mz.sns.service.UserDetailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,17 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserDetailController {
 
   private final UserDetailService userDetailService;
+  private final UserDetails securityUserDetails;
 
   public UserDetailController(UserDetailService userDetailService) {
     this.userDetailService = userDetailService;
+    this.securityUserDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
   }
 
-  @GetMapping("{userSeq}")
-  public ResponseEntity<UserDetailDto> findByUserSeq(@PathVariable Long userSeq) {
-    if (userSeq == null || userSeq < 1) {
+  @GetMapping("")//todo client에서 userseq 를 줄수있는 방법이 없습니다.
+  public ResponseEntity<UserDetailDto> findByUserSeq() {
+    var userEmail = securityUserDetails.getUsername();
 
-    }
-    var optionalUserDetailDto = userDetailService.findById(userSeq);
+    var optionalUserDetailDto = userDetailService.findByUserSeq(1L);
     return ResponseEntity.status(HttpStatus.OK).body(optionalUserDetailDto);
   }
 
@@ -40,10 +43,10 @@ public class UserDetailController {
   @PatchMapping("")
   public ResponseEntity<UserDetailDto> update(@RequestBody UserDetailDto
       userDetailDto) {
-    return ResponseEntity.status(HttpStatus.OK).body(userDetailService.saveOne(userDetailDto));
+    return ResponseEntity.status(HttpStatus.OK).body(userDetailService.updateByUserSeq(userDetailDto));
   }
 
-  @DeleteMapping("{userSeq}")
+  @DeleteMapping("{userSeq}")//todo client에서 userseq 를 줄수있는 방법이 없습니다.
   public ResponseEntity<String> delete(@PathVariable Long userSeq) {
     var result = userDetailService.deleteByUserSeq(userSeq);
     return ResponseEntity.status(HttpStatus.OK).body("Your detail is deleted Successfully for :" + result + " rows.");
--- src/main/java/kr/co/mz/sns/service/UserDetailService.java
+++ src/main/java/kr/co/mz/sns/service/UserDetailService.java
@@ -20,9 +20,10 @@ public class UserDetailService {
   public UserDetailService(UserDetailRepository userDetailRepository, ModelMapper modelMapper) {
     this.userDetailRepository = userDetailRepository;
     this.modelMapper = modelMapper;
+
   }
 
-  public UserDetailDto findById(Long userSeq) {
+  public UserDetailDto findByUserSeq(Long userSeq) {
     if (!userDetailRepository.existsByUserSeq(userSeq)) {
       throw new EmptyResultDataAccessException("Expected 1 result, but none returned.", 1);
     }
@@ -30,13 +31,23 @@ public class UserDetailService {
         UserDetailDto.class);
   }
 
+//  public UserDetailDto findByEmail(String email) {
+//
+//    if (!userDetailRepository.existsByEmail(userSeq)) {
+//      throw new EmptyResultDataAccessException("Expected 1 result, but none returned.", 1);
+//    }
+//    return modelMapper.map(userDetailRepository.findById(userSeq).orElseGet(UserDetailEntity::new),
+//        UserDetailDto.class);
+//  }
+
+
   public UserDetailDto saveOne(UserDetailDto userDetailDto) {
-    Long userSeq = userDetailDto.getUserSeq();
+    var userSeq = userDetailDto.getUserSeq();
     if (userDetailRepository.existsByUserSeq(userSeq)) {
       throw new DataIntegrityViolationException("UserDetail with user_seq " + userSeq + " already exists.");
     }
     var userDetailEntity = modelMapper.map(userDetailDto, UserDetailEntity.class);
-    var savedEntity = userDetailRepository.save(userDetailEntity);
+    var savedEntity = userDetailRepository.save(userDetailEntity);//todo insertfailed exception
     return modelMapper.map(savedEntity, UserDetailDto.class);
   }
 
@@ -54,16 +65,8 @@ public class UserDetailService {
     return userDetailRepository.deleteByUserSeq(userSeq);
   }
 
-  public UserDetailDto updateUserDetail(UserDetailDto userDetailDto) {
-    var userDetailEntity = modelMapper.map(userDetailDto, UserDetailEntity.class);
-    var updatedUserDetailEntity = userDetailRepository.save(userDetailEntity);
-    // 완벽하게 dto 랑 맞지 않으니까.왜? default 로 생성되는 db설정들 외 기타 요인들 떄문.
-    // 업데이트 후 업데이트 된 entitiy 를 다시 dto로 만들어서 보내준다.
-    return modelMapper.map(updatedUserDetailEntity, UserDetailDto.class);
-  }
+  // 완벽하게 dto 랑 맞지 않으니까.왜? default 로 생성되는 db설정들 외 기타 요인들 떄문.
+  // 업데이트 후 업데이트 된 entitiy 를 다시 dto로 만들어서 보내준다.
 
-  public UserDetailDto toDto(UserDetailEntity userDetailEntity) {
-    return modelMapper.map(userDetailEntity, UserDetailDto.class);
-  }
 
 }
