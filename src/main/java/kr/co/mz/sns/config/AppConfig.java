67f91952e0df44ed7551c723de4269fda598077b
mz01-junghunee <cjswovkdnj12@naver.com>
2023-07-25 15:14:11 +0900

    Delete UserDetail register to Bean

--- src/main/java/kr/co/mz/sns/config/AppConfig.java
+++ src/main/java/kr/co/mz/sns/config/AppConfig.java
@@ -3,8 +3,6 @@ package kr.co.mz.sns.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Configuration
 public class AppConfig {
@@ -12,10 +10,5 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
-    }
-
-    @Bean
-    public UserDetails userDetails() {
-        return (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
