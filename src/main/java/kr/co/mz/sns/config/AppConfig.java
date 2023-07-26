f824b820e781e9dbf87fd7079f1a88a39b1b0725
mz01-junghunee <cjswovkdnj12@naver.com>
2023-07-26 20:27:03 +0900

    Update Auditing

--- src/main/java/kr/co/mz/sns/config/AppConfig.java
+++ src/main/java/kr/co/mz/sns/config/AppConfig.java
@@ -1,14 +1,22 @@
 package kr.co.mz.sns.config;
 
+import kr.co.mz.sns.auditing.AuditorAwareImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public AuditorAwareImpl auditorAware() {
+        return new AuditorAwareImpl();
     }
 }
