e7d2511549513369b983033eeff3c6be9600533c
mz01-omega <omega@mz.co.kr>
2023-07-27 12:53:23 +0900

    add util for current email

--- src/main/java/kr/co/mz/sns/controller/user/UserDetailController.java
+++ src/main/java/kr/co/mz/sns/controller/user/UserDetailController.java
@@ -1,11 +1,10 @@
 package kr.co.mz.sns.controller.user;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import kr.co.mz.sns.dto.user.InsertUserDetailDto;
 import kr.co.mz.sns.dto.user.UpdateUserDetailDto;
-import kr.co.mz.sns.dto.user.UserFindByEmailDto;
 import kr.co.mz.sns.service.UserDetailService;
+import kr.co.mz.sns.util.CurrentUserInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,40 +22,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/public/user_detail")
 public class UserDetailController {
 
-    private final UserDetailService userDetailService;
+  private final UserDetailService userDetailService;
+  private final CurrentUserInfo currentUserInfo;
 
-    @GetMapping
-    public ResponseEntity<InsertUserDetailDto> findByEmail(@Valid @RequestBody UserFindByEmailDto userFindByEmailDto) {
-        return ResponseEntity
-            .ok(
-                userDetailService.findByEmail(userFindByEmailDto.getEmail())
-            );
-    }
+  @GetMapping
+  public ResponseEntity<InsertUserDetailDto> findByEmail() {
+    return ResponseEntity
+        .ok(
+            userDetailService.findByEmail(
+                currentUserInfo.getEmail()
+            )
+        );
+  }
 
-    @PostMapping
-    public ResponseEntity<InsertUserDetailDto> write(@RequestBody InsertUserDetailDto insertUserDetailDto) {
-        return ResponseEntity
-            .status(HttpStatus.CREATED)
-            .body(
-                userDetailService.saveOne(insertUserDetailDto)
-            );
-    }
+  @PostMapping
+  public ResponseEntity<InsertUserDetailDto> insert(@RequestBody InsertUserDetailDto insertUserDetailDto) {
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(
+            userDetailService.saveOne(insertUserDetailDto)
+        );
+  }
 
-    @PutMapping
-    public ResponseEntity<InsertUserDetailDto> update(@RequestBody UpdateUserDetailDto
-        updateUserDetailDto) {
-        return ResponseEntity
-            .ok(
-                userDetailService.updateByUserSeq(updateUserDetailDto)
-            );
-    }
+  @PutMapping
+  public ResponseEntity<InsertUserDetailDto> update(@RequestBody UpdateUserDetailDto
+      updateUserDetailDto) {
+    return ResponseEntity
+        .ok(
+            userDetailService.updateByUserSeq(updateUserDetailDto)
+        );
+  }
 
-    @DeleteMapping("/{userSeq}")
-    public ResponseEntity<String> delete(@NotNull @PathVariable Long userSeq) {
-        var result = userDetailService.deleteByUserSeq(userSeq);
-        return ResponseEntity
-            .ok(
-                "Your detail is deleted Successfully for :" + result + " rows."
-            );
-    }
+  @DeleteMapping("/{userSeq}")
+  public ResponseEntity<String> delete(@NotNull @PathVariable Long userSeq) {
+    var result = userDetailService.deleteByUserSeq(userSeq);
+    return ResponseEntity
+        .ok(
+            "Your detail is deleted Successfully for :" + result + " rows."
+        );
+  }
 }
